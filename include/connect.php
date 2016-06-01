dbeab42407dd2fb6541147aeb05e062248980ebf
TPaiboon <freeze_observation@hotmail.com>
2016-06-01 14:08:39 +0700

    connect

--- include/connect.php
+++ include/connect.php
@@ -2,8 +2,8 @@
 	$HOST_NAME = "127.0.0.1";
 	$DB_NAME = "Proj_Notebook";
 	$CHAR_SET = "charset=utf8"; 
-	$USERNAME = "thapanonz";     
-	$PASSWORD = "104";  
+	$USERNAME = "paiboon";     
+	$PASSWORD = "123";  
  
 	try {
 		$db = new PDO('mysql:host='.$HOST_NAME.';dbname='.$DB_NAME.';'.$CHAR_SET,$USERNAME,$PASSWORD);
