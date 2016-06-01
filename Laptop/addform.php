2e22febfaa7a2daf08fed59cb7498d2a5621949d
thapanonz <thapanonz@hotmail.com>
2016-06-01 14:56:17 +0700

    save Before merge

--- Laptop/addform.php
+++ Laptop/addform.php
@@ -23,7 +23,28 @@
 				<div class="row">
 					<div class="col-md-11">						
 						<h1>ป้อนรายการเครื่องเช่าใหม่</h1>
+						
+						<form class="form-horizontal" role="form">
+					    <div class="form-group">
+					      <label class="col-md-4">หมายเลขเครื่องเช่า:</label>
+					      <div calss="col-md-4"><input type="text" class="form-control" name="nbCode" placeholder="CC-008">
+					    </div>
+					    <div class="form-group">
+					      <label>ซีเรียลเครื่องเช่า:</label>
+					      <input type="text" class="form-control" name="nbSerial" placeholder="PF07ZHKB">
+					    </div>
+					    <div class="form-group">
+					      <label>ยี่ห้อ/รุ่น:</label>
+					      <input type="text" class="form-control" name="nbSerial" placeholder="PF07ZHKB">
+					       <input type="text" class="form-control" name="nbSerial" placeholder="PF07ZHKB">
+					        <input type="text" class="form-control" name="nbSerial" placeholder="PF07ZHKB">
+					    </div>
+					    
 
+					    <button type="submit" class="btn btn-success">บันทึก</button>
+					    <button type="reset" class="btn btn-info">ยกเลิก</button>
+					  </form>
+			
 					</div>
 					
 				</div>
