af45dcc85d31ae18e03399e26065aa2fa889b236
thapanonz <thapanonz@hotmail.com>
2016-06-01 11:45:29 +0700

    Change banner color

--- include/banner.php
+++ include/banner.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="background-color: #F90757">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="background-color: black">
 			ddddddddddd
 		</div>
 	</div>
