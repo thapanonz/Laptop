8697bee7a0ac3b06aeaeb9fb138b5cfc1d40e3bf
thapanonz <thapanonz@hotmail.com>
2016-06-14 14:04:11 +0700

    ..

--- include/connect.php
+++ include/connect.php
@@ -1,13 +1,11 @@
 <?php
 	$HOST_NAME = "127.0.0.1";
-<<<<<<< HEAD
+
 	$DB_NAME = "laptop";
-=======
 	$DB_NAME = "proj_notebook";
->>>>>>> Thapanon
 	$CHAR_SET = "charset=utf8"; 
-	$USERNAME = "acadmin";     
-	$PASSWORD = "zxc123**";  
+	$USERNAME = "thapanonz";     
+	$PASSWORD = "104";  
  
 	try {
 		$db = new PDO('mysql:host='.$HOST_NAME.';dbname='.$DB_NAME.';'.$CHAR_SET,$USERNAME,$PASSWORD);
