1a41b0e4a82499a77a0826ee7a06e01407016ed9
thapanonz <thapanonz@hotmail.com>
2016-06-23 15:21:01 +0700

    .

--- Rent/agreement.php
+++ Rent/agreement.php
@@ -32,11 +32,12 @@ require'../include/connect.php';
 </head>
 <body>
 
-		<div id="wrapper">
+<div id="wrapper">
 	<div id="content">
 	<?php	
 			$Id=$_GET["Id"];
-			$sql = $db->prepare ("SELECT * FROM reportlist WHERE Id_rent=".$Id);
+			$sql = $db->prepare ("SELECT *, DATEDIFF(appointlap, rentlap) AS days 
+				                  FROM reportlist WHERE Id_rent=".$Id);
 			$sql->execute();
 			$sql->setFetchMode(PDO::FETCH_ASSOC);
 			while ($row = $sql->fetch()) { ?>
@@ -50,17 +51,16 @@ require'../include/connect.php';
 			<p>สัญญาฉบับนี้ทำขึ้น ณ ศูนย์คอมพิวเตอร์ มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่<br>
 			เมื่อวันที่ 
 			<span><?php echo DateThai($row["rentlap"]) ?></span> ระหว่างศูนย์คอมพิวเตอร์ มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตหาดใหญ่ ซึ่งต่อไปในสัญญานี้เรียกว่า "ผู้ให้เช่า" ฝ่ายหนึ่งกับ 
-			<span><?php echo $row["prename"].$row["firstname"].' '.$row["lastname"] ?></span><br> หมายเลขประจำตัวประชาชน       <span><?php echo $row["Id_customer"] ?></span>        ประเภท <span><?php echo settype1($row["type"]) ?></span>สังกัดคณะ/หน่วยงาน <span><?php echo $row["faculty"] ?></span>  รหัสนักศึกษา/บุคลากร  <span><?php echo $row["passport"] ?></span><br>เบอร์โทรศัพท์   <span><?php echo $row["phone"] ?></span>        อีเมล์     <span><?php echo $row["email"] ?></span></p>
+			<span style="margin-left:25px;"><?php echo $row["prename"].$row["firstname"].' '.$row["lastname"] ?></span><br> หมายเลขประจำตัวประชาชน <span><?php echo $row["Id_customer"] ?></span>        ประเภท <span><?php echo settype1($row["type"]) ?></span>สังกัดคณะ/หน่วยงาน <span><?php echo $row["faculty"] ?></span>  รหัสนักศึกษา/บุคลากร  <span><?php echo $row["passport"] ?></span><br>เบอร์โทรศัพท์   <span><?php echo $row["phone"] ?></span>  อีเมล์ <span><?php echo $row["email"] ?></span></p>
 			
-			<p style="line-height: 20px">ซึ่งต่อไปในสัญญานี้เรียกว่า "ผู้เช่า" อีกฝ่ายหนึ่ง คู่สัญญาได้ตกลงกันมีข้อความ ดังต่อไปนี้<br>ผู้เช่าได้เช่าเครื่องคอมพิวเตอร์ Notebook รหัสบาร์โค้ด  <span><?php echo $row["nbCode"] ?></span>       Serial Number    <span><?php echo $row["nbSerial"] ?></span><br>ยี่ห้อ/รุ่น <span><?php echo $row["nbBrand"] ?></span>   และคุณลักษณะจำเพาะอื่นๆ ดังนี้ 
+			<p style="line-height: 20px">ซึ่งต่อไปในสัญญานี้เรียกว่า "ผู้เช่า" อีกฝ่ายหนึ่ง คู่สัญญาได้ตกลงกันมีข้อความ ดังต่อไปนี้<br>ผู้เช่าได้เช่าเครื่องคอมพิวเตอร์ Notebook รหัสบาร์โค้ด  <span><?php echo $row["nbCode"] ?></span>  Serial Number <span><?php echo $row["nbSerial"] ?></span><br>ยี่ห้อ/รุ่น <span><?php echo $row["nbBrand"] ?></span>   และคุณลักษณะจำเพาะอื่นๆ ดังนี้ 
 
 			<div style="padding:0 7px 0 7px; border:1px solid #000;"><?php echo $row["nbDetails"] ?></div></p>
-
-			<p>ผู้เช่าได้ทำสัญญาเช่าและรับเครื่องไปเมื่อวันที่   <span><?php echo DateThai($row["rentlap"]) ?></span>         มีระยะเวลาเช่า ______________ วัน
-		โดยจะครบกำหนดส่งคืนในวันที่    <span><?php echo DateThai($row["appointlap"]) ?></span>        ก่อนเวลา 13:00 น.</p>
+			
+			<p>ผู้เช่าได้ทำสัญญาเช่าและรับเครื่องไปเมื่อวันที่ <span><?php echo DateThai($row["rentlap"]) ?></span>  มีระยะเวลาเช่า <span> <?php echo $row["days"] ?></span> วัน<br>โดยจะครบกำหนดส่งคืนในวันที่ <span><?php echo DateThai($row["appointlap"]) ?></span>  ก่อนเวลา 13:00 น.</p>		
 		
-		<div>
-<div style="float:left; width:45%; height: 167px; padding:0 0 0 3px; border:1px solid #000;">
+	<div>
+<div style="float:left; width:45%; height: 163px; padding:0 0 0 3px; border:1px solid #000;">
 <pre style="line-height: 18px">
 <b>วันเวลาให้เช่าและรับคืน</b>
 (1) ให้เช่าวันจันทร์-ศุกร์ เวลา 09:00-16:00 น.
@@ -71,7 +71,7 @@ require'../include/connect.php';
 (5) กรณีวันครบกำหนดคืนตรงกับวันหยุด 
 ให้นำมาคืนก่อนวันหยุด</pre>
 			</div>
-<div style="float:left; width:51%; height: 167px; padding:0 0 0 3px; margin-left: 10px; border:1px solid #000;">
+<div style="float:left; width:51%; height: 163px; padding:0 0 0 3px; margin-left: 10px; border:1px solid #000;">
 <pre style="line-height: 18px">
 <b>ความรับผิดชอบของผู้เช่า</b>
 (1) ตัวเครื่องหรืออุปกรณ์ประกอบ ได้รับความเสียหาย
@@ -82,11 +82,11 @@ require'../include/connect.php';
 (3) ผู้เช่าทำลายพาร์ทิชั่นของฮาร์ดดิสก์ต้องชดใช้เงิน 300 บาท
 (4) สติ๊กเกอร์ของชิ้นส่วนอุปกรณ์มีการแกะหรือฉีกขาด 
 ต้องชดใช้เงิน 500 บาท</pre>
-			</div>
 		</div>
+	</div>
 
 			<div>
-<div style="float:left; width:45%; height: 112px; padding:0 0 0 3px; margin-top: 7px; border:1px solid #000;">
+<div style="float:left; width:45%; height: 110px; padding:0 0 0 3px; margin-top: 7px; border:1px solid #000;">
 <pre style="line-height: 18px">
 <b>รายการที่เช่า</b>
 (1) เครื่องคอมพิวเตอร์ Notebook
@@ -95,7 +95,7 @@ require'../include/connect.php';
 (4) กระเป๋าเป้สะพาย
 (5) ปลั๊ก 2 ขา</pre>
 			</div>
-<div style="float:left; width:51%; height: 112px; padding:0 0 0 3px; margin-top: 7px; margin-left: 10px; border:1px solid #000;">
+<div style="float:left; width:51%; height: 110px; padding:0 0 0 3px; margin-top: 7px; margin-left: 10px; border:1px solid #000;">
 <pre style="line-height: 18px">
 <b>อัตราค่าเช่าและค่าปรับ</b>
 (1) ค่าเช่านักศึกษา 50 บาท/วัน บุคลากร 150 บาท/วัน
@@ -105,7 +105,7 @@ require'../include/connect.php';
 			</div>
 		</div>
 
-		<p style="line-height: 20px; padding-top: 50px" >คู่สัญญาได้อ่านข้อความในสัญญาเช่า รวมทั้งประกาศศูนย์คอมพิวเตอร์ เรื่องข้อกำหนดการเช่าเครื่องคอมพิวเตอร์<br> ชนิดพกพา (Notebook) และเข้าใจข้อความโดยละเอียดแล้ว ยอมรับเงื่อนไขทุกประการ จึงได้ลงลายมือชื่อไว้เป็นหลักฐาน</p>
+		<p style="line-height: 22px; padding-top: 50px" >คู่สัญญาได้อ่านข้อความในสัญญาเช่า รวมทั้งประกาศศูนย์คอมพิวเตอร์ เรื่องข้อกำหนดการเช่าเครื่องคอมพิวเตอร์<br> ชนิดพกพา (Notebook) และเข้าใจข้อความโดยละเอียดแล้ว ยอมรับเงื่อนไขทุกประการ จึงได้ลงลายมือชื่อไว้เป็นหลักฐาน</p>
 
 	<div style="padding-top: 20px">
 <div style="float:left; width:45%; height: 167px; padding:0 0 0 3px; border:0px solid #000;">
@@ -120,9 +120,9 @@ require'../include/connect.php';
 </pre>
 			</div>
 		</div>
-
 	<?php } ?>
 	</div>	
+
 
 </div>
 		
