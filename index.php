769a3d97c7686fac9bf9eb49cadb0a57b98962ad
thapanonz <thapanonz@hotmail.com>
2016-06-25 01:50:38 +0700

    .

--- index.php
+++ index.php
@@ -19,22 +19,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-9">						
-				<?php
-					$sql = $db->prepare("SELECT COUNT(Id) AS sumlaptop FROM notebook WHERE nbStatus='rdy'");
-					$sql->execute();
-					$sql->setFetchMode(PDO::FETCH_ASSOC);
-					if ($row = $sql->fetch()) { ?>
-						เครื่องพร้อมให้เช่าจำนวน <span class='label label-success'> <?php echo $row["sumlaptop"] ?></span> เครื่อง
-					} ?>						
-					</div>	
-					<div class="col-xs-5">						
 				
-					</div>	
-					<div class="col-xs-4">						
-					<a href="acadmin/rent/index.php" class="btn btn-primary">บันทึกการเช่า</a>
-					<a href="acadmin/rent/return.php" class="btn btn-primary">บันทึกการคืน</a>
-					<a href="#" class="btn btn-primary">สรุปข้อมูลรายวัน</a>
-					</div>	
 							
 				</div>
 			</div>
